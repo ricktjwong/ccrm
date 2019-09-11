a9af081b3d45082d182cfc32d00546f845ab7be6
LoneRifle <LoneRifle@users.noreply.github.com>
2019-09-11 16:48:32 +0800

    Remove redundant semicolon

--- api/utils/types.ts
+++ api/utils/types.ts
@@ -1,4 +1,4 @@
-import { Model, FindOptions, CreateOptions } from 'sequelize';
+import { Model, FindOptions, CreateOptions } from 'sequelize'
 
 export interface IFindAll<T extends Model<T>> {
   findAll(options?: FindOptions): Promise<T[]>
